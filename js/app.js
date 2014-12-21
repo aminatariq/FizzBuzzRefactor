9d348885c7ed846807596fbfcabe88311e62802f
Amina Tariq <aminatariq@gmail.com>
2014-12-21 17:36:22 -0500

    Added prompt line

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+var input = prompt("Please enter a number between 1 to 100 to start playing the game:");
+
 var i;
 var fizz;
 var buzz;
 
-for (i=0;i<=100;i++) {
+for (i=0;i<=input;i++) {
 	
 	fizz="";
 	buzz="";
