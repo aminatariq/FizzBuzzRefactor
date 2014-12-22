6e8caca7c545c2a52cf0952f347cf92e0aa5cd15
Amina Tariq <aminatariq@gmail.com>
2014-12-21 21:21:59 -0500

    Added warning text boxes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-var input = prompt("Please enter a number between 1 to 100 to start playing the game:");
+var guess = prompt("Please enter a number between 1 to 100 to start playing the game:"); 
+
+if (guess % 1 != 0) {
+			prompt("Please enter a whole number, not a fraction or text");
+   		 } else if (guess < 1 || guess > 100) {
+    		prompt("Please choose between 1 and 100");
+    	} else {
+    	}
+
+//Original FizzBuzz code below//
 
 var i;
 var fizz;
 var buzz;
 
-for (i=0;i<=input;i++) {
+for (i=0;i<=guess;i++) {
 	
 	fizz="";
 	buzz="";
